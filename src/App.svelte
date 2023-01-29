a97e0794233af2c9de07e66725315e1a30a2dfd2
Nico PFC y Luna <nicopfcyluna@dhcp-10-250-195-147.harvard.edu>
2023-01-29 14:13:57 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  let todos = [];
+  let newTodo = "";
+
+  function addTodo() {
+    todos = [...todos, newTodo];
+    newTodo = "";
+  }
+
+  function deleteTodo(index) {
+    todos = todos.filter((_, i) => i !== index);
+  }
+</script>
+
+<input bind:value={newTodo}>
+<button on:click={addTodo}>Add Todo</button>
+
+<ul>
+  {#each todos as todo, index}
+    <li>
+      {todo}
+      <button on:click={() => deleteTodo(index)}>Delete</button>
+    </li>
+  {/each}
+</ul> 
